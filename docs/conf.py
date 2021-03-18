ca423b62efc26c3c68c9a21ff42da259b93837f3
Julie.Prestopnik <jpresto@ucar.edu>
2021-03-17 18:01:36 -0600

    Fixed typos, added content, modified release date format

--- docs/conf.py
+++ docs/conf.py
@@ -19,23 +19,18 @@ print(sys.path)
 # -- Project information -----------------------------------------------------
 
 project = 'METexpress'
-
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Strong, B., Pierce, R., Smith, M.B., Hagerty, V., Hamilton, Jeff'
 
 # The stable version, displayed on the front page of the PDF
-version = '3.0.1'
+version = '4.0.0'
 verinfo = version
 
 # The full version, including alpha/beta/rc tags
 release = f'{version}'
-
-release_year = '2020'
-
-release_date = f'{release_year}1001'
-
+release_year = '2021'
+release_date = f'{release_year}-03-31'
 copyright = f'{release_year}, {author}'
-
 release_monthyear = datetime.strptime(release_date, '%Y%m%d').strftime('%B %Y')
 
 # -- General configuration ---------------------------------------------------
