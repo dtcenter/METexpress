46b31c1d6505fc203dc2a65915713831c18c0afa
Keith Searight <searight@ucar.edu>
2021-03-19 13:21:22 -0600

    Test latex_* settings in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'master.tex', 'METexpress User\'s Guide v4.0',
+    (master_doc, 'master.tex', f'METexpress User\'s Guide v{version}',
                  'Bonny Strong\\Randy Pierce\\Molly B. Smith\\Venita Hagerty\\Jeff Hamilton\\', 'report')
     ]
     
