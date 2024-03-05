2d9595ca36a34172e33a91e7f7dc6de9b15d5c97
Molly Smith <molly.b.smith@noaa.gov>
2024-03-04 17:41:57 -0700

    Update docs for release

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,6 @@ author = 'UCAR/DTC, NOAA, CU/CIRES, and CSU/CIRA'
 # To rotate this list of authors for each official release,
 # move the first author to the end of the list
 CURRENT_AUTHORS = [
-    'Matthew Wandishin',
     'Molly Smith',
     'Randy Pierce'
     'Jeff Hamilton',
@@ -33,6 +32,7 @@ CURRENT_AUTHORS = [
     'Ian McGinnis',
     'Gopa Padmanabhan',
     'Bonny Strong',
+    'Matthew Wandishin',
 ]
 
 # list of former contributors who are no longer involved with the project
@@ -61,13 +61,13 @@ for index, author_name in enumerate(CURRENT_AUTHORS + FORMER_AUTHORS):
 author_list = ', '.join(formatted_authors)
 
 # The stable version, displayed on the front page of the PDF
-version = '5.2.2'
+version = '5.2.3'
 verinfo = version
 
 # The full version, including alpha/beta/rc tags
 release = f'{version}'
 release_year = '2024'
-release_date = f'{release_year}0126'
+release_date = f'{release_year}0308'
 copyright = f'{release_year}, {author}'
 release_monthyear = datetime.strptime(release_date, '%Y%m%d').strftime('%B %Y')
 
