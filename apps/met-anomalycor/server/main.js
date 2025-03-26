722ff9519083010d0998974b88fcbb6b44c848ff
Molly Smith <molly.b.smith@noaa.gov>
2025-03-26 11:54:35 -0600

    Removed aggregation from met-anomalycor

--- apps/met-anomalycor/server/main.js
+++ apps/met-anomalycor/server/main.js
@@ -481,7 +481,6 @@ const doCurveParams = function () {
         for (let j = 0; j < sourceArr.length; j += 1) {
           sourceArr[j] = sourceArr[j].replace(/'|\[|\]/g, "");
         }
-        sourceArr.unshift("Any truth dataset");
 
         const forecastLengths = rows[i].fcst_orig;
         const forecastLengthArr = forecastLengths
@@ -512,7 +511,6 @@ const doCurveParams = function () {
         for (let j = 0; j < imsArr.length; j += 1) {
           imsArr[j] = imsArr[j].replace(/'|\[|\]/g, "");
         }
-        imsArr.unshift("All methods");
 
         const scales = rows[i].gridpoints;
         const scalesArr = scales
@@ -521,7 +519,6 @@ const doCurveParams = function () {
         for (let j = 0; j < scalesArr.length; j += 1) {
           scalesArr[j] = scalesArr[j].replace(/'|\[|\]/g, "");
         }
-        scalesArr.unshift("All scales");
 
         const { descrs } = rows[i];
         const descrsArr = descrs
